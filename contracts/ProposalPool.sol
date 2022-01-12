86d4ac46ec378176d2e3de0e2af7fb0348460ee5
Julian <juliandinkins2@yahoo.com>
2022-01-12 13:06:11 -0500

    Update ProposalPool.sol

--- contracts/ProposalPool.sol
+++ contracts/ProposalPool.sol
@@ -1,6 +1,44 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.9;
+
+contract SafeMath {
+
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a + b;
+        require(c >= a, "SafeMath: addition overflow");
+        return c;    }
+
+    function safeAdd(uint a, uint b) internal pure returns (uint c) {
+        c = a + b;
+        require(c >= a);
+    }
+    function safeSub(uint a, uint b) internal pure returns (uint c) {
+        require(b <= a);
+        c = a - b;
+    }
+
+    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+        if (a == 0) {
+            return 0;}
+            uint256 c = a * b;
+            require(c / a == b, "SafeMath: multiplication overflow");
+            return c;}
+
+    function div(uint256 a, uint256 b) internal pure returns (uint256) {
+        require(b > 0, "SafeMath: division by zero");
+        uint256 c = a / b;
+        return c;    }
+}
+
+interface InterfaceDigi {
+    function balanceOf(address tokenOwner) external view returns (uint balance);
+    function totalSupply() external view returns (uint);
+    function transfer(address receiver, uint tokens) external returns (bool success);
+    function transferFrom(address sender, address receiver, uint tokens) external returns (bool success);
+}
+
 interface InterfaceDaoGov {
   function getDaoGovAddress() external view returns (address);
-  function getDigitradeAddress() external view returns (address);
 }
 
  contract ProposalPool is SafeMath{
@@ -30,6 +68,8 @@ interface InterfaceDaoGov {
         uint availiableTokens = fundStrength() * 200;
         return availiableTokens;
     }
+    
+    //Interface to DaoGov etc....
 
     
 
